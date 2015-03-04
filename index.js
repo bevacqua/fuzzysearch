acb1aed0f2e3cd155334ee45531934dd874eaa62
Rodrigo Alviani <rodrigoalviani@gmail.com>
2015-03-04 13:40:55 -0300

    Linting

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 'use strict';
 
 function fuzzysearch (query, text) {
-  var i;
-  var character;
-  var currentIndex;
-  var lastIndex = -1;
-  var tlen = text.length;
-  var qlen = query.length;
-  if (qlen > tlen) {
-    return false;
-  }
-  if (qlen === tlen && query === text) {
-    return true;
-  }
-  if (text.indexOf(query) > lastIndex) {
-    return true;
-  }
+  var i
+    , character
+    , currentIndex
+    , lastIndex = -1
+    , tlen = text.length
+    , qlen = query.length;
+
+  if (qlen > tlen) return false;
+  if ((qlen === tlen && query === text) || (text.indexOf(query) > lastIndex)) return true;
+
   for (i = 0; i < qlen; i++) {
     character = query[i];
     currentIndex = text.indexOf(character, lastIndex + 1);
-    if (currentIndex === -1) {
-      return false;
-    }
+    if (currentIndex === -1) return false;
     lastIndex = currentIndex;
   }
+
   return true;
 }
 
