07e5031b3c2b4a64496d83ed3c3915a57db34657
Larry Battle <larry.battle.tech@gmail.com>
2015-03-04 15:56:08 -0600

    Switched to function style

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
-var escapeRegExp = function(str) {
-  return str.replace(/[{}()*+?.^$|[\]]/g, '\\$&');
-};
-var toReg = function(query) {
-  var x = String(query).split('').map(function(c) {
-    return escapeRegExp(c);
-  }).join('.*?');
+function escapeRegExp(str) {
+  return str.replace(/[-{}()*+?.^$|[\]]/g, '\\$&');
+}
+
+function toReg(query) {
+  var x = String(query).split('').map(escapeRegExp).join('.*?');
   x = '(?:' + x + ')';
   return new RegExp(x);
-};
-var fuzzysearch = function(query, text) {
+}
+
+function fuzzysearch(query, text) {
   if (typeof query === 'string' && typeof text === 'string' && query.length && text.length) {
     return toReg(query).test(text);
   }
   return false;
-};
+}
 module.exports = fuzzysearch;
