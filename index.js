524897103f5a9c2f0f4b4e899bf0c7f30104696b
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-03-09 01:33:56 -0300

    relevant len variable names

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict';
 
 function fuzzysearch (needle, haystack) {
-  var tlen = haystack.length;
-  var qlen = needle.length;
-  if (qlen > tlen) {
+  var hlen = haystack.length;
+  var nlen = needle.length;
+  if (nlen > hlen) {
     return false;
   }
-  if (qlen === tlen) {
+  if (nlen === hlen) {
     return needle === haystack;
   }
-  outer: for (var i = 0, j = 0; i < qlen; i++) {
+  outer: for (var i = 0, j = 0; i < nlen; i++) {
     var nch = needle.charCodeAt(i);
-    while (j < tlen) {
+    while (j < hlen) {
       if (haystack.charCodeAt(j++) === nch) {
         continue outer;
       }
