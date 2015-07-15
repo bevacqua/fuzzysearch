ec3a6e0e55e712b3a7893e65438a4745a7654928
Peter Kaske <p.kaske@gmail.com>
2015-07-15 14:43:53 +0200

    Added bower.json file. Added some logic to make this lib compatible with amd and require.js.

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-'use strict';
+;(function() {
+  'use strict';
 
-function fuzzysearch (needle, haystack) {
-  var hlen = haystack.length;
-  var nlen = needle.length;
-  if (nlen > hlen) {
-    return false;
-  }
-  if (nlen === hlen) {
-    return needle === haystack;
-  }
-  outer: for (var i = 0, j = 0; i < nlen; i++) {
-    var nch = needle.charCodeAt(i);
-    while (j < hlen) {
-      if (haystack.charCodeAt(j++) === nch) {
-        continue outer;
-      }
+  function fuzzysearch (needle, haystack) {
+    var hlen = haystack.length;
+    var nlen = needle.length;
+    if (nlen > hlen) {
+      return false;
     }
-    return false;
+    if (nlen === hlen) {
+      return needle === haystack;
+    }
+    outer: for (var i = 0, j = 0; i < nlen; i++) {
+      var nch = needle.charCodeAt(i);
+      while (j < hlen) {
+        if (haystack.charCodeAt(j++) === nch) {
+          continue outer;
+        }
+      }
+      return false;
+    }
+    return true;
   }
-  return true;
-}
 
-module.exports = fuzzysearch;
+  if (typeof module !== 'undefined' && typeof exports === 'object') {
+    module.exports = fuzzysearch;
+  } else if (typeof define === 'function' && define.amd) {
+    define(function() {
+      return fuzzysearch;
+    });
+  } else {
+    this.fuzzysearch = fuzzysearch;
+  }
+
+}).call(function() {
+  return this || (typeof window !== 'undefined' ? window : global);
+}());
