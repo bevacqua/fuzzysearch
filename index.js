532325db63e3bebec6ad4ac20dc68e1023a94d84
Stu Kabakoff <sakabako@gmail.com>
2015-12-01 15:31:34 -0500

    curry fuzzysearch

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 'use strict';
 
 function fuzzysearch (needle, haystack) {
-  var hlen = haystack.length;
-  var nlen = needle.length;
-  if (nlen > hlen) {
-    return false;
-  }
-  if (nlen === hlen) {
-    return needle === haystack;
-  }
-  outer: for (var i = 0, j = 0; i < nlen; i++) {
-    var nch = needle.charCodeAt(i);
-    while (j < hlen) {
-      if (haystack.charCodeAt(j++) === nch) {
-        continue outer;
-      }
+  function test (haystack) {
+    var hlen = haystack.length;
+    var nlen = needle.length;
+    if (nlen > hlen) {
+      return false;
     }
-    return false;
+    if (nlen === hlen) {
+      return needle === haystack;
+    }
+    outer: for (var i = 0, j = 0; i < nlen; i++) {
+      var nch = needle.charCodeAt(i);
+      while (j < hlen) {
+        if (haystack.charCodeAt(j++) === nch) {
+          continue outer;
+        }
+      }
+      return false;
+    }
+    return true;
   }
-  return true;
+
+  if (typeof haystack === 'undefined') {
+    return test;
+  }
+
+  return test(haystack);
 }
 
 module.exports = fuzzysearch;
--- test/fuzzysearch.js
+++ test/fuzzysearch.js
@@ -12,3 +12,17 @@ test('fuzzysearch should match expectations', function (t) {
   t.equal(fuzzysearch('lw', 'cartwheel'), false);
   t.end();
 });
+
+test('fuzzysearch should curry', function (t) {
+  var test = fuzzysearch('car');
+  t.equal(test('cartwheel'), true);
+  t.equal(test('boatwheel'), false);
+
+  var things = ['cartwheel', 'boatwheel', 'wheels on the bus', 'car tire'];
+  var result = things.map(test);
+  t.deepEqual(result, [true, false, false, true]);
+
+  var otherResult = things.filter(test);
+  t.deepEqual(otherResult, ['cartwheel', 'car tire']);
+  t.end();
+});
